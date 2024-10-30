21a1592b3689e62a59279705affafd942eeb1e30
1999Snehal <snehallagad84@gmail.com>
2024-10-30 12:04:03 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+ class StreamFilter
+{
+    public static void main(String arg[])
+    {
+        List<Integer>list = Arrays.asList(5,10,15,35,20);
+
+        List<Integer>filteredList = list.stream().filter(i->i%2==0).toList();
+
+        System.out.println(filteredList);
     }
 }
